1b37d0be09100cd6d116477a88af6a1d6354141e
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2023-06-13 15:41:47 -0300

    Update Forca.py

--- Jogos/Forca.py
+++ Jogos/Forca.py
@@ -4,6 +4,33 @@ def forca():
     print("Ola, bem vindo ao jogo da forca")
     print("*******************************")
 
+    palavra_secreta = "banana"
+    #Atribui o valor FALSO para o enforcou
+    enforcou = False
+    #Atribui o valor FALSO para o acertou
+    acertou = False
+    rodadas = 3
+    tentativas = 10
+    #enquanto o jogador não acertar e não ter enforcado, o jogo continuara
+    while(not acertou and not enforcou):
+        print("Jogando...")
+        
+        #Variavel de chute para cada letra escrita
+        chute = input("Digite uma letra: ")
+        #Strip ira retirar o ignorar o espaçamento no inicio e no fim
+        chute = chute.strip()
+
+        #Posição da letra
+        posicao = 0
+        #PARA letra DENTRO DE palavra_secreta
+        for letra in palavra_secreta:
+            #se chute for igual a letra
+            #.upper() para que ele considere caso seja escrito em maiusculo
+            if(chute.upper() == letra.upper()):
+                #Mostra a letra e a sua posição dentro da palavra
+                print("Encontrei a letra {} na posição {}".format(letra, posicao))
+            posicao = posicao + 1    
+
     print("Fim de jogo")
 
 #Essa parte do codigo fara com que conseguimos executar esse arquivo tanto pelo "Jogos.py" quanto abrindo diretamente ele
